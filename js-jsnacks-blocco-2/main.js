2ea00fc4459b2a3f7b38caee1f08160dc522e98b
rota99C <rotaboolean@gmail.com>
2021-10-25 11:33:19 +0200

    creato arrey di oggetti con proprietà nome e peso

---
+++ js-jsnacks-blocco-2/main.js
@@ -0,0 +1,23 @@
+//Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+let bicilette = [
+    {
+        name: "MERIDA",
+        peso: "45kg",
+
+    },
+    {
+        name: "TREK",
+        peso: "30kg",
+    },
+    {
+        name: "KONA",
+        peso: "25kg",
+    },
+    {
+        name: "SCOTT",
+        peso: "25kg",
+    }
+]
+
+//Stampare a schermo la bici con peso minore. 
