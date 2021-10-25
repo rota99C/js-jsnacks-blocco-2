f4dab87b6e521ee318575a71826ba65d4b52c117
rota99C <rotaboolean@gmail.com>
2021-10-25 12:20:23 +0200

    creato arrey di squadre con diverse proprietà

---
+++ main.js
@@ -0,0 +1,23 @@
+//Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+let Squadre = [
+    {
+        name: "juventus",
+        puntiFatti: "2",
+
+    },
+    {
+        name: "Atalanta",
+        puntiFatti: "3",
+    },
+    {
+        name: "Roma",
+        puntiFatti: "1",
+    },
+    {
+        name: "RomaT",
+        puntiFatti: "6",
+    }
+]
+
+
